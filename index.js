963d5f3263f3b774bf797b5a38148beef568bb04
Andrey <pachalia2009@yandex.com>
2024-11-19 11:24:12 +0300

    Задание 4. Добавил и подключил плагины webpack. Создал 2 DOM элемента h1 и img и вывел их на страницу.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+import './index.css'
+import js from './assets/js.png'
+
+const body = document.querySelector('body')
+
+const head = document.createElement('h1')
+const img = document.createElement('img')
+img.src = js
+head.innerText = 'I love JavaScript'
+body.append(head, img)
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -6,5 +7,39 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'index.html'),
+        }),
+    ],
+    devServer: {
+        port: 8080,
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        targets: "defaults",
+                        presets: [
+                            ['@babel/preset-env']
+                        ]
+                    }
+                }
+            }
+        ],
+    },
 }
